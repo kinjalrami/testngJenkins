b03f1f2fb5d2bdf04d721565e63b057f96520328
kinjalrami <kinjal.rami19@gmail.com>
2020-02-09 16:41:44 -0600

    Testing Commit1

---
+++ src/test/java/com/itexps/JenkinsTestng/DriverUtils.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.itexps.JenkinsTestng;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+/**
+ *
+ * @author RANGANI2
+ */
+public class DriverUtils {
+     public static WebDriver getChromeDriver() {//web driver is interface
+        //setup chrome
+        System.setProperty("webdriver.chrome.driver", "C:\\data\\chromedriver.exe");//(type,value)
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        return new ChromeDriver(capabilities);//chrome driver implements webdriver
+
+    }
+
+    public static void main(String[] args) {
+        getChromeDriver();
+
+    }
+
+}
